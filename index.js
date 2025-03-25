79c3c0fa02233b04eef900a314637ca05f50f029
pauline muthoni <kinyanjuipauline69@gmail.com>
2025-03-25 21:25:05 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const messages = []; 
+
+  for (let i = 0; i < names.length; i++) {
+
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message); 
+  }
+
+  return messages; 
+}
+
+const cards = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(cards);
+
+function countDown(start) {
+  while (start >= 0) {
+
+    console.log(start);
+    start--; 
+  }
+}
+
+countDown(10);
